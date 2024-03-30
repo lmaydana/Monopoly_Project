1f9d3aea12aa936aefd9e7649a1cf403ae841a9d
oswald1511 <omaldonado@fi.uba.ar>
2024-03-30 14:23:03 -0300

    agregada la consola

---
+++ src/main/java/org/example/Consola.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Consola {
+    public Consola(int cantidad_jugadores){
+        bienvenida();
+    }
+
+    public void bienvenida(){
+        System.out.println("¡Bienvenido al Monopoly, donde los tratos se hacen y se rompen más rápido que una promesa" +
+                " electoral! Prepárate para ser un magnate inmobiliario o caer en la bancarrota intentándolo. " +
+                "¡Que comience el juego y que la estrategia más astuta gane!");
+    }
+
+
+}
+
+
