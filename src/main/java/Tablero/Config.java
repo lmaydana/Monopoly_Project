fccac8c4d5bdd1a865bb66c54519be99bae3a3a1
Ariel Salmaso <ariel_salmaso@hotmail.com>
2024-03-31 20:01:15 -0300

    Agrego Config y inicializa Tablero

---
+++ src/main/java/Tablero/Config.java
@@ -0,0 +1,16 @@
+package Tablero;
+
+public class Config {
+    //definir bien la cantidad de cada una
+    protected int cantTotalCasillas = 50;
+    protected int cantCasillasPaso = 16;
+    protected int cantCasillasPropiedades = 22;
+    protected int cantCasillasLoterias = 4;
+    protected int cantCasillasMulta= 2;
+    protected int cantCasillasIrCarcel = 1;
+    protected int cantCasillasCarcel = 1;
+    protected int cantCasillasTransporte = 4;
+
+    protected double cantDineroJugadorInicial = 1500;
+    //esto no se si conviene tenerlo aca
+}
--- src/main/java/Tablero/Tablero.java
+++ src/main/java/Tablero/Tablero.java
@@ -1,9 +1,62 @@
 package Tablero;
-
+import java.util.Random;
 public class Tablero {
     private ListaCircular<Casillero> tablero;
+    private Config config;
+    public Tablero(){
+        this.tablero = new ListaCircular<Casillero>();
+        this.config = new Config();
+        this.inicializarTablero();
+    }
 
-    public Casillero    mover(int origen, int pasos){
+    //TODO: cambiar por los distintos tipos de casilleros
+    private Casillero devolerCasilleroRandom(Random random){
+        Casillero casillero = null;
+        int numeroRandom = random.nextInt(5);
+        switch (numeroRandom){
+            case 0:
+                //de paso
+                casillero = new Casillero();
+            case 1:
+                //propiedad
+                casillero = new Casillero();
+            case 2:
+                //loteria
+                casillero = new Casillero();
+            case 3:
+                //Multa
+                casillero = new Casillero();
+            case 4:
+                //transporte
+                casillero = new Casillero();
+        }
+        return casillero;
+    }
+    
+    //TODO: Que verifique que se agreguen solamente 4 de loteria, 2 de multa, etc.
+    private void inicializarTablero() {
+        Random random = new Random();
+        int posicionCarcel = random.nextInt(config.cantTotalCasillas);
+        int posicionIrCarcel;
+        do {
+            posicionIrCarcel = random.nextInt(config.cantTotalCasillas);
+        } while (posicionCarcel == posicionIrCarcel);
+
+        for (int i = 0; i < config.cantTotalCasillas; i++) {
+            if(i == posicionCarcel){
+                //casillero de carcel
+                this.tablero.append(new Casillero());
+            } else if (i == posicionIrCarcel){
+                //casillero de ir Carcel
+                this.tablero.append(new Casillero());
+            }
+            Casillero casilleroAgregar = devolerCasilleroRandom(random);
+            this.tablero.append(casilleroAgregar);
+
+        }
+    }
+
+    public Casillero mover(int origen, int pasos){
         return this.tablero.mover(origen, pasos);
     }
 
